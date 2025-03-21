3d8903f56256aebdaa2a97be806aee2f486fd6e3
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-03-21 13:18:29 -0300

    items colors logic added

--- src/components/TodoPost.tsx
+++ src/components/TodoPost.tsx
@@ -5,9 +5,19 @@ type Props = {
 
 export const TodoPost = ({ task }: Props) => {
   //TODO get random number to randomize the color
+
+  const colorNumber = Math.floor(Math.random() * 5);
+  const colors = [
+    "bg-light-blue",
+    "bg-light-pink",
+    "bg-light-yellow",
+    "bg-light-green",
+    "bg-light-violet",
+  ];
+
   return (
     <article
-      className={`flex flex-col w-full h-20 bg-amber-300  p-2 font-body rounded-lg`}
+      className={`${colors[colorNumber]} flex flex-col w-full h-20 p-2 font-body rounded-lg`}
     >
       <h1>{task}</h1>
     </article>
